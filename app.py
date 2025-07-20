0fcf067dc46525afef1ac3833a7e67031da6e978
Kat C <kathychu1999@gmail.com>
2025-07-20 14:39:28 -0700

    running html working

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from datetime import datetime, timedelta
 # Database Setup
 #################################################
 # Create engine using the `hawaii.sqlite` database file
-database_path = "Resources/hawaii.sqlite"
+database_path = "/Applications/Data Analysis Bootcamp/UofM-VIRT-DATA-PT-06-2024-U-LOLC/02_Challenges/10_SQL_Advanced/weather-data-flask-api/Resources/hawaii.sqlite"
 engine = create_engine(f"sqlite:///{database_path}")
 
 # Initialize the Flask app
@@ -35,23 +35,29 @@ Station = Base.classes.station
 @app.route('/')
 def home():
     return (
-    "Welcome to the Hawaii Weather API!<br/><br/>"
-    "Available Routes:<br/>"
-    "<a href='/api/v1.0/precipitation'>/api/v1.0/precipitation</a><br/>"
-    "<a href='/api/v1.0/stations'>/api/v1.0/stations</a><br/>"
-    "<a href='/api/v1.0/tobs'>/api/v1.0/tobs</a><br/>"
-    "<a href='/api/v1.0/2017-01-01'>/api/v1.0/&lt;start&gt;</a><br/>"
-    "<a href='/api/v1.0/2017-01-01/2017-08-01'>/api/v1.0/&lt;start&gt;/&lt;end&gt;</a><br/>"
-)
+        "Welcome to the Hawaii Weather API!<br/><br/>"
+        "Available Routes:<br/>"
+        "<a href='/api/v1.0/precipitation'>/api/v1.0/precipitation</a><br/>"
+        "<a href='/api/v1.0/stations'>/api/v1.0/stations</a><br/>"
+        "<a href='/api/v1.0/tobs'>/api/v1.0/tobs</a><br/>"
+        "<a href='/api/v1.0/2017-01-01'>/api/v1.0/&lt;start&gt;</a><br/>"
+        "<a href='/api/v1.0/2017-01-01/2017-08-01'>/api/v1.0/&lt;start&gt;/&lt;end&gt;</a><br/>"
+    )
 
 @app.route('/api/v1.0/precipitation')
 def precipitation():
-    # Get the last 12 months of data
-    twelve_months_ago = datetime.now() - timedelta(days=365)
+    # Find the most recent date in the data
+    most_recent_date = db.session.query(func.max(Measurement.date)).scalar()
+    most_recent_date = datetime.strptime(most_recent_date, '%Y-%m-%d')
+
+    # Go 12 months back from the most recent date
+    twelve_months_ago = most_recent_date - timedelta(days=365)
+
+    # Query data
     results = db.session.query(Measurement.date, Measurement.prcp).\
         filter(Measurement.date >= twelve_months_ago).all()
 
-    # Convert the results to a dictionary
+    # Format as dictionary
     precipitation_data = {date: prcp for date, prcp in results}
     return jsonify(precipitation_data)
 
@@ -63,36 +69,42 @@ def stations():
 
 @app.route('/api/v1.0/tobs')
 def tobs():
-    # Query the dates and temperature observations of the most-active station for the previous year
+    # Query the most-active station
     most_active_station = db.session.query(Measurement.station).\
-        group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).first()
-    
-    most_active_station = most_active_station[0]
-    
-    twelve_months_ago = datetime.now() - timedelta(days=365)
+        group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).first()[0]
+
+    # Get the latest date in the data
+    most_recent_date = db.session.query(func.max(Measurement.date)).scalar()
+    most_recent_date = datetime.strptime(most_recent_date, '%Y-%m-%d')
+    twelve_months_ago = most_recent_date - timedelta(days=365)
+
     results = db.session.query(Measurement.date, Measurement.tobs).\
         filter(Measurement.station == most_active_station).\
         filter(Measurement.date >= twelve_months_ago).all()
 
-    # Create a list of temperature observations
+    # Format results
     tobs_data = [{date: tobs} for date, tobs in results]
     return jsonify(tobs_data)
 
 @app.route('/api/v1.0/<start>')
 @app.route('/api/v1.0/<start>/<end>')
 def stats(start, end=None):
-    # Convert the start and end date strings to datetime objects
     start_date = datetime.strptime(start, '%Y-%m-%d')
-    
+
     if end:
         end_date = datetime.strptime(end, '%Y-%m-%d')
-        results = db.session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-            filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+        results = db.session.query(
+            func.min(Measurement.tobs),
+            func.avg(Measurement.tobs),
+            func.max(Measurement.tobs)
+        ).filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
     else:
-        results = db.session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-            filter(Measurement.date >= start_date).all()
+        results = db.session.query(
+            func.min(Measurement.tobs),
+            func.avg(Measurement.tobs),
+            func.max(Measurement.tobs)
+        ).filter(Measurement.date >= start_date).all()
 
-    # Create a dictionary for the results
     stats_data = {
         "TMIN": results[0][0],
         "TAVG": results[0][1],
